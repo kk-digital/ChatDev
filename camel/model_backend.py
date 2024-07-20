1b6f03d133005f8d3b2db4ba93c08457632bb2ac
pedrodearaujoalvarenga <100007896+pedrodearaujoalvarenga@users.noreply.github.com>
2024-07-20 19:18:32 -0300

    VllmModel implementation #1

--- camel/model_backend.py
+++ camel/model_backend.py
@@ -144,6 +144,73 @@ class OpenAIModel(ModelBackend):
                 raise RuntimeError("Unexpected return from OpenAI API")
             return response
 
+class VllmModel(ModelBackend):
+    r"""OpenAI API in a unified ModelBackend interface."""
+
+    def __init__(self, model_type: ModelType, model_config_dict: Dict) -> None:
+        super().__init__()
+        self.model_type = model_type
+        self.model_config_dict = model_config_dict
+
+    def run(self, *args, **kwargs):
+        string = "\n".join([message["content"] for message in kwargs["messages"]])
+        encoding = tiktoken.encoding_for_model(self.model_type.value)
+        num_prompt_tokens = len(encoding.encode(string))
+        gap_between_send_receive = 15 * len(kwargs["messages"])
+        num_prompt_tokens += gap_between_send_receive
+
+        num_max_token = 4096
+        if openai_new_api:
+            # Experimental, add base_url
+            if BASE_URL:
+                client = openai.OpenAI(
+                    api_key=OPENAI_API_KEY,
+                    base_url=BASE_URL,
+                )
+            else:
+                client = openai.OpenAI(
+                    api_key=OPENAI_API_KEY
+                )
+
+            num_max_completion_tokens = num_max_token - num_prompt_tokens
+            self.model_config_dict['max_tokens'] = num_max_completion_tokens
+
+            response = client.chat.completions.create(*args, **kwargs, model=self.model_type.value,
+                                                      **self.model_config_dict)
+
+            cost = prompt_cost(
+                self.model_type.value,
+                num_prompt_tokens=response.usage.prompt_tokens,
+                num_completion_tokens=response.usage.completion_tokens
+            )
+
+            log_visualize(
+                "**[OpenAI_Usage_Info Receive]**\nprompt_tokens: {}\ncompletion_tokens: {}\ntotal_tokens: {}\ncost: ${:.6f}\n".format(
+                    response.usage.prompt_tokens, response.usage.completion_tokens,
+                    response.usage.total_tokens, cost))
+            if not isinstance(response, ChatCompletion):
+                raise RuntimeError("Unexpected return from OpenAI API")
+            return response
+        else:
+            num_max_completion_tokens = num_max_token - num_prompt_tokens
+            self.model_config_dict['max_tokens'] = num_max_completion_tokens
+
+            response = openai.ChatCompletion.create(*args, **kwargs, model=self.model_type.value,
+                                                    **self.model_config_dict)
+
+            cost = prompt_cost(
+                self.model_type.value,
+                num_prompt_tokens=response["usage"]["prompt_tokens"],
+                num_completion_tokens=response["usage"]["completion_tokens"]
+            )
+
+            log_visualize(
+                "**[OpenAI_Usage_Info Receive]**\nprompt_tokens: {}\ncompletion_tokens: {}\ntotal_tokens: {}\ncost: ${:.6f}\n".format(
+                    response["usage"]["prompt_tokens"], response["usage"]["completion_tokens"],
+                    response["usage"]["total_tokens"], cost))
+            if not isinstance(response, Dict):
+                raise RuntimeError("Unexpected return from OpenAI API")
+            return response
 
 class StubModel(ModelBackend):
     r"""A dummy model used for unit tests."""
@@ -187,6 +254,8 @@ class ModelFactory:
             model_class = OpenAIModel
         elif model_type == ModelType.STUB:
             model_class = StubModel
+        elif model_type == ModelType.VLLM:
+            model_class = VllmModel
         else:
             raise ValueError("Unknown model")
 
